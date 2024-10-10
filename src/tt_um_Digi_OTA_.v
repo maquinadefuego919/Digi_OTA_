c575c102dd460843014cd6487789dd1d631a6cb9
Abnerpfff1 <parra.abner@uabc.edu.mx>
2024-10-10 16:57:49 -0700

    Update tt_um_Digi_OTA_.v

--- src/tt_um_Digi_OTA_.v
+++ src/tt_um_Digi_OTA_.v
@@ -20,5 +20,32 @@ module tt_um_Digi_OTA_ (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+   wire Vip, Vin, Out;
+
+    assign Vip = ui_in[0];
+    assign Vin = ui_in[1];
+    
+    assign uo_out[0]  = Out;  
+    assign uo_out[7:1] = 7'b0000000; 
+
+    wire INn, INp, INn_CMP, INp_CMP, CMP, EN, not_EN, Op, On, INn_AND, INp_AND; //internals nets 
+    not IV1(INn, Vip);    
+    not INV2(INn_CMP,CMP);
+    not IV3(INp, Vin);
+    not INV4(INp_CMP,CMP);
+
+    nor NOR1(Op, INn, INn_CMP);
+    nor NOR2(On, INp, INp_CMP);
+    
+    //not IV5(Op, INn_AND);
+    //not IV6(On, INp_AND);
+    
+    xor XOR1(EN, Op, On);
+    
+    not IV7(not_EN, EN);
+    notif1 IT1(CMP, not_EN, Op);  
+    
+    bufif1 BT1(Out, EN, Op);  
+
     
 endmodule
