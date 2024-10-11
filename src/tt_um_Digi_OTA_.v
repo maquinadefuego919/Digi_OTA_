49e0b652074bf785b2ec7803c9774aa108ee373a
Abnerpfff1 <parra.abner@uabc.edu.mx>
2024-10-10 18:12:29 -0700

    Update tt_um_Digi_OTA_.v

--- src/tt_um_Digi_OTA_.v
+++ src/tt_um_Digi_OTA_.v
@@ -26,9 +26,9 @@ assign Vip = ua[0];
 assign Vin = ua[1];
     
 assign ua[2]  = Out;  
-//assign uo_out[7:0] = 8'b00000000; 
+assign uo_out[7:0] = 8'b00000000; 
 //assign ui_in[7:0] = 8'b00000000;
-    
+    assign ua[5:3]= 3'b000;
 
 wire INn, INp, INn_CMP, INp_CMP, CMP, EN, not_EN, Op, On, INn_AND, INp_AND; //internals nets 
 not IV1(INn, Vip);    
