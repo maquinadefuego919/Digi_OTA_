048e005a6f9112275af005f6cd9a908fbfc02837
Abnerpfff1 <parra.abner@uabc.edu.mx>
2024-10-11 20:01:47 -0700

    Update tt_um_Digi_OTA_.v

--- src/tt_um_Digi_OTA_.v
+++ src/tt_um_Digi_OTA_.v
@@ -26,9 +26,9 @@ assign ua[0] = Vip;
 assign ua[1] = Vin;
     
 assign ua[2]  = Out;  
-  //  assign ua[5:3]= 3'b000;
-//assign uo_out[7:0] = 8'b00000000; 
-//assign ui_in[7:0] = 8'b00000000;
+    assign ua[5:3]= 3'b000;
+assign uo_out[7:0] = 8'b00000000; 
+assign ui_in[7:0] = 8'b00000000;
 
  wire INn, INp, INn_CMP, INp_CMP, CMP, EN, not_EN, Op, On, INn_AND, INp_AND; //internals nets 
 not IV1(INn, Vip);    
@@ -51,7 +51,7 @@ not IV1(INn, Vip);
     
 
    
-  //assign uio_out = 0;
+  assign uio_out = 0;
     assign uio_oe  = 0;
     
   // List all unused inputs to prevent warnings
