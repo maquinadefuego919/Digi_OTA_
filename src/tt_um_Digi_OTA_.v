28f2a5f9ee7a81511ea00ac68c965aac1c7a7895
Abnerpfff1 <parra.abner@uabc.edu.mx>
2024-10-11 19:57:37 -0700

    Update tt_um_Digi_OTA_.v

--- src/tt_um_Digi_OTA_.v
+++ src/tt_um_Digi_OTA_.v
@@ -49,37 +49,12 @@ not IV1(INn, Vip);
     
     bufif1 BT1(Out, EN, Op);   
     
-//wire Out, Vip, Vin;
-  // All output pins must be assigned. If not used, assign to 0.
-//assign [0] uo_out  = Out;  // Example: ou_out is the sum of ui_in and uio_in
-//assign uo_out[7:1] = 7'b0000000:
-    
-    
-//assign Vip= ui_in [0]   ;
-//assign Vin= ui_in [1];
 
-//wire INn, INp, CMP, EN, not_EN, Op, On, Pr, INn_CMP, INp_CMP, INp_NOR, INn_NOR; //internals nets
-//not IV1 (INn, Vip);
-//not IV2 (INn, CMP);
-//not IV3 (INp, Vin);
-//not IV4 (INp, CMP);
-
-    
-//nor NOR1 (INn_NOR, INn, INn_CMP):
-//nor NOR2 (INp_NOR, INp, Inp_CMP);
-    
-//not IV5 (Op, INn_CMP);
-//not IV6 (On, INp_CMP);
-    
-//not IV7 (not_EN, EN);
-//xor XOR1 (EN, Op, On);
-//bufif1 BT1 (Pr, EN, Op);
-//notif1 IT1 (CMP, not_EN, Op);
    
   //assign uio_out = 0;
-    //assign uio_oe  = 0;
+    assign uio_oe  = 0;
     
   // List all unused inputs to prevent warnings
- // wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, 1'b0};
     
 endmodule
