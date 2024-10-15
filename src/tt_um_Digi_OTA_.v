64b7ecd41d7b2b917c0e4bddc3ee8693d7e91c81
Abnerpfff1 <parra.abner@uabc.edu.mx>
2024-10-14 17:13:58 -0700

    Update tt_um_Digi_OTA_.v

--- src/tt_um_Digi_OTA_.v
+++ src/tt_um_Digi_OTA_.v
@@ -26,9 +26,9 @@ wire Vip, Vin, Out;
   assign ua[1] = Vin;
     
 assign Out  = ua[2];  
-assign ua[5:3] = 3'b000;
+//assign ua[5:3] = 3'b000;
 //assign uo_out[7:0] = 8'b00000000; 
-assign ui_in[7:0] = 8'b00000000;
+//assign ui_in[7:0] = 8'b00000000;
 
  wire INn, INp, INn_CMP, INp_CMP, CMP, EN, not_EN, Op, On; //internals nets 
 not IV1(INn, Vip);    
@@ -55,6 +55,33 @@ bufif1 BT1(Out, EN, Op);
    // assign uio_oe  = 0;
     
   // List all unused inputs to prevent warnings
-wire _unused = &{ena, clk, rst_n, 1'b0};
-    
+//wire _unused = &{ena, clk, rst_n, 1'b0};
+
+  assign uo_out[0] = VGND;
+  assign uo_out[1] = VGND;
+  assign uo_out[2] = VGND;
+  assign uo_out[3] = VGND;
+  assign uo_out[4] = VGND;
+  assign uo_out[5] = VGND;
+  assign uo_out[6] = VGND;
+  assign uo_out[7] = VGND;
+
+  assign uio_out[0] = VGND;
+  assign uio_out[1] = VGND;
+  assign uio_out[2] = VGND;
+  assign uio_out[3] = VGND;
+  assign uio_out[4] = VGND;
+  assign uio_out[5] = VGND;
+  assign uio_out[6] = VGND;
+  assign uio_out[7] = VGND;
+
+  assign uio_oe[0] = VGND;
+  assign uio_oe[1] = VGND;
+  assign uio_oe[2] = VGND;
+  assign uio_oe[3] = VGND;
+  assign uio_oe[4] = VGND;
+  assign uio_oe[5] = VGND;
+  assign uio_oe[6] = VGND;
+  assign uio_oe[7] = VGND;
+  
 endmodule
